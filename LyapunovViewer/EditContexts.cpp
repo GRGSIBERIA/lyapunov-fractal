935a8c0d85b962039cec9c019abe4a6779874430
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-06-19 16:22:29 +0900

    マクロを使って複雑度を下げる

--- LyapunovViewer/EditContexts.cpp
+++ LyapunovViewer/EditContexts.cpp
@@ -12,19 +12,20 @@ void CreateEdit(HWND hWnd, const int x, const int y, const int w, const int h, L
 
 void EditContext::initialize(HWND hWnd, const int x, const int y, const int skip, LPARAM lParam)
 {
+#define INITEDIT(PARAM) CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, PARAM)
     int c = 0;
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Width);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Height);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Sequence);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, N);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, InitX);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Amin);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Amax);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Bmin);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Bmax);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Func);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Const1);
-    CreateEdit(hWnd, x, y + skip * c++, 100, 25, lParam, Const2);
+    INITEDIT(Width);
+    INITEDIT(Height);
+    INITEDIT(Sequence);
+    INITEDIT(N);
+    INITEDIT(InitX);
+    INITEDIT(Amin);
+    INITEDIT(Amax);
+    INITEDIT(Bmin);
+    INITEDIT(Bmax);
+    INITEDIT(Func);
+    INITEDIT(Const1);
+    INITEDIT(Const2);
 }
 
 // https://www.wabiapp.com/WabiSampleSource/windows/string_to_wstring.html
